f03c1cf5d8a519be1fc01eda8c49cdda20052c56
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2020-03-02 22:48:40 +0700

    Add models draft

--- stocks/analysis/models.py
+++ stocks/analysis/models.py
@@ -3,5 +3,15 @@ from django.db import models
 # Create your models here.
 
 
-# class Stock(models.Model):
-#     pass
+class Stock(models.Model):
+    figi = models.CharField()
+    ticker = models.CharField()
+
+
+class Quote(models.Model):
+    stock = models.ForeignKey(Stock, models.CASCADE, related_name='quotes')
+    # datetime
+    # price_open
+    # price_high
+    # price_low
+    # price_close
