2f78d494f71503b36b49c7ffff3d7d45c3991508
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2020-03-03 19:53:11 +0700

    Add django-background-tasks

--- stocks/settings.py
+++ stocks/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'stocks.analysis'
+    'stocks.analysis',
+    'background_task'
 ]
 
 MIDDLEWARE = [
