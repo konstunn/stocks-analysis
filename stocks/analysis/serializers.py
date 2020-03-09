e2838774dc890951d73e1a78833f99ea25e9c3ff
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2020-03-10 00:29:17 +0700

    Add TaskProxySerializer

--- stocks/analysis/serializers.py
+++ stocks/analysis/serializers.py
@@ -1,8 +1,9 @@
-
+from openapi_genclient import CandleResolution
+from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 
-from stocks.analysis.models import Instrument, Candle
+from stocks.analysis.models import Instrument, Candle, TaskProxy
 
 
 class InstrumentSerializer(ModelSerializer):
@@ -35,3 +36,39 @@ class CandleSerializer(ModelSerializer):
             'c',
             'figi'
         )
+
+
+ACTION_CHOICE = ('get_instruments', 'get_candles')
+
+
+class TaskProxySerializer(serializers.ModelSerializer):
+    action = serializers.ChoiceField(choices=ACTION_CHOICE, required=True)
+    interval = serializers.ChoiceField(choices=CandleResolution.allowable_values, required=False)
+    _from = serializers.DateTimeField(source='from', required=False)
+    to = serializers.DateTimeField(required=False)
+    figi = serializers.CharField(required=False)
+
+    class Meta:
+        model = TaskProxy
+        fields = (
+            'from',
+            'to',
+            'interval',
+            'action',
+            'figi'
+        )
+
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+        action = attrs['action']
+
+        if action == 'get_instruments':  # TODO: make a enum
+            if len(attrs) > 1:
+                raise ValidationError('extra fields {} for action = {}'.format(attrs.pop('action'), action))
+        elif action == 'get_candles':
+            # TODO: heavy validation to exclude from to overlapping between tasks goes here
+            pass
+
+    def create(self, validated_data):
+        # TODO: schedule task
+        pass
