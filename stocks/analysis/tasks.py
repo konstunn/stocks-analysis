56a414ad322828ea3972f9e171e6b37f75ff5081
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2020-03-10 22:47:08 +0700

    Get rid of transaction in tasks

--- stocks/analysis/tasks.py
+++ stocks/analysis/tasks.py
@@ -6,12 +6,13 @@ from typing import List
 from datetime import datetime, timedelta
 
 from django.conf import settings
-from django.db import transaction
 
 from openapi_client.openapi import sandbox_api_client, SandboxOpenApi
+
 from openapi_genclient.models import \
     MarketInstrumentListResponse, MarketInstrumentList, MarketInstrument, SearchMarketInstrument
-from openapi_genclient.models import Candles, Candle, CandlesResponse, CandleResolution
+
+from openapi_genclient.models import Candles, Candle, CandleResolution
 from openapi_genclient.models import Currency
 from openapi_genclient.exceptions import ApiException
 
@@ -20,8 +21,14 @@ from stocks.analysis.models import Instrument
 from stocks.analysis.serializers import InstrumentSerializer, CandleSerializer
 
 
+DELAY_LIMIT = 120
+DELAY_MULTIPLIER = 3
+
+
 def get_next_delay(last_delay):
-    next_delay = last_delay * 3
+    next_delay = last_delay * DELAY_MULTIPLIER
+    if next_delay > DELAY_LIMIT:
+        next_delay = DELAY_LIMIT
     return next_delay
 
 
@@ -51,13 +58,12 @@ def get_instruments(currency=Currency.RUB):
     instruments: List[MarketInstrument] = payload.instruments
     instruments = list(filter(lambda i: i.currency == currency, instruments))
 
-    with transaction.atomic():
-        for instrument in instruments:
-            data = instrument.to_dict()
-            if not Instrument.objects.filter(figi=instrument.figi):
-                instrument = InstrumentSerializer(data=data)
-                instrument.is_valid(raise_exception=True)
-                instrument.save()
+    for instrument in instruments:
+        data = instrument.to_dict()
+        if not Instrument.objects.filter(figi=instrument.figi):
+            instrument = InstrumentSerializer(data=data)
+            instrument.is_valid(raise_exception=True)
+            instrument.save()
 
 
 GRANULARITY_INTERVAL_TO_MAX_OVERALL_INTERVAL_MAP = {
@@ -102,33 +108,34 @@ def get_candles(figi: str, _from: datetime, to: datetime, granularity_interval: 
 
     tinkoff_client: SandboxOpenApi = sandbox_api_client(settings.TINKOFF_INVESTMENTS_SANDBOX_OPEN_API_TOKEN)
 
-    with transaction.atomic():
+    instrument = Instrument.objects.filter(figi=figi)
+    if not instrument:
+        market_search_by_figi_with_retry_on_rate_limits = \
+            retry_on_rate_limits_exception(tinkoff_client.market.market_search_by_figi_get)
 
-        instrument = Instrument.objects.filter(figi=figi)
-        if not instrument:
-            market_search_by_figi_with_retry_on_rate_limits = \
-                retry_on_rate_limits_exception(tinkoff_client.market.market_search_by_figi_get)
+        response: MarketInstrumentListResponse = market_search_by_figi_with_retry_on_rate_limits(figi)
+        instrument: SearchMarketInstrument = response.payload
+        instrument_serializer = InstrumentSerializer(data=instrument.to_dict())
+        instrument_serializer.is_valid(raise_exception=True)
+        instrument_serializer.save()
 
-            response: MarketInstrumentListResponse = market_search_by_figi_with_retry_on_rate_limits(figi)
-            instrument: SearchMarketInstrument = response.payload
-            instrument_serializer = InstrumentSerializer(data=instrument.to_dict())
-            instrument_serializer.is_valid(raise_exception=True)
-            instrument_serializer.save()
+        instrument = instrument_serializer.instance
 
-        for start, end in pairwise(time_points):
+    for start, end in pairwise(time_points):
 
-            market_candles_get_with_retry_on_rate_limits = \
-                retry_on_rate_limits_exception(tinkoff_client.market.market_candles_get_with_http_info)
+        market_candles_get_with_retry_on_rate_limits = \
+            retry_on_rate_limits_exception(tinkoff_client.market.market_candles_get_with_http_info)
 
-            response, status, headers = market_candles_get_with_retry_on_rate_limits(figi,
-                                                                                     start,
-                                                                                     end,
-                                                                                     granularity_interval)
+        response, status, headers = market_candles_get_with_retry_on_rate_limits(figi,
+                                                                                 start,
+                                                                                 end,
+                                                                                 granularity_interval)
 
-            payload: Candles = response.payload
-            candles: List[Candle] = payload.candles
+        payload: Candles = response.payload
+        candles: List[Candle] = payload.candles
 
-            for candle in candles:
+        for candle in candles:
+            if not models.Candle.objects.filter(instrument=instrument, time=candle.time, interval=candle.interval):
                 candle_serializer = CandleSerializer(data=candle.to_dict())
                 candle_serializer.is_valid(raise_exception=True)
                 candle_serializer.save()
